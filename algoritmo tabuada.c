beeaa53d569c4b8b63d3fae46b6ea316fc667641
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2024-02-29 10:56:42 -0300

    Add files via upload

---
+++ algoritmo tabuada.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main(){
+	 setlocale(LC_ALL, "");
+	 
+	int i;
+	int numero;
+	
+	printf("Digite um número:  ");
+	scanf("%d", &numero);
+	
+	for (i = 1; i <= 10; i++){
+		printf(" %d x %d = %d \n", numero, i, numero*i);
+	}
+	
+	return 0;
+}
